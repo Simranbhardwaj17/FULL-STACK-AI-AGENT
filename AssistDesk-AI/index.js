818bc6bf4149449b3da411bea4bf96d3e8f3d1ff
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-07-16 18:08:29 +0530

    Updated

--- AssistDesk-AI/index.js
+++ AssistDesk-AI/index.js
@@ -6,7 +6,7 @@ import userRoutes from "./routes/user.js";
 import ticketRoutes from "./routes/ticket.js";
 import { inngest } from "./inngest/client.js";
 import { onUserSignup } from "./inngest/functions/on-signup.js";
-import { onTicketCreated } from "./inngest/functions/on-signup.js";
+import { onTicketCreated } from "./inngest/functions/on-ticket-create.js";
 import dotenv from "dotenv";
 
 dotenv.config()
@@ -29,6 +29,6 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(PORT, () => console.log("Server at http://localhost:3000"));
+    app.listen(PORT, () => console.log(`Server at http://localhost:${PORT}`));
   })
   .catch((err) => console.error("MongoDB error: ", err));
